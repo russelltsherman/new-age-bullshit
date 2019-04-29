1ab2cc0f2345927bc74282e41d6442b23241b72d
Russell <russell.t.sherman@gmail.com>
2019-04-29 06:42:03 -0500

    add execution using pico

--- app.go
+++ app.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"os/exec"
 	"time"
 
@@ -11,47 +10,45 @@ import (
 func main() {
 	for {
 		sentence := bs.Sentence()
-		// log.Println(sentence)
 
 		// executing using osx say
 		// say is good sound but only available on OSX
 		// cmd := exec.Command("say", sentence)
-		// err := cmd.Run()
-		// if err != nil {
-		// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-		// }
+		// cmd.Run()
 
 		// executing using espeak
 		// voices are a bit screechy
 		// playback on pi zero frequently stops and slurs
 		// cmd := exec.Command("espeak", "-ven+f3", "-k5", "-s150", sentence)
-		// err := cmd.Run()
-		// if err != nil {
-		// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-		// }
+		// cmd.Run()
 
 		// executing using Cepstral voice
 		// Cepstral is certainly the most natural sounding voice
 		// playback on pi zero frequently stops and slurs
 		// cmd := exec.Command("aoss", "swift", sentence)
-		// err := cmd.Run()
-		// if err != nil {
-		// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-		// }
+		// cmd.Run()
 
 		// executing using festival
 		// voice quality is ok
+		// playback on pi zero is very stable
+		// c1 := exec.Command("echo", sentence)
+		// c2 := exec.Command("festival", "--tts")
+		// r, w := io.Pipe()
+		// c1.Stdout = w
+		// c2.Stdin = r
+		// c1.Start()
+		// c2.Start()
+		// c1.Wait()
+		// w.Close()
+		// c2.Wait()
+
+		// executing using Pico
+		// voice quality is ok
 		// playback on pi zero is the most stable of all tested
-		c1 := exec.Command("echo", sentence)
-		c2 := exec.Command("festival", "--tts")
-		r, w := io.Pipe()
-		c1.Stdout = w
-		c2.Stdin = r
-		c1.Start()
-		c2.Start()
-		c1.Wait()
-		w.Close()
-		c2.Wait()
+		c1 := exec.Command("pico2wav", "-l", "en-GB", "-w", "/tmp/output.wav", sentence)
+		c1.Run()
+		c2 := exec.Command("aplay", "/tmp/output.wav")
+		c2.Run()
 
 		time.Sleep(1000 * time.Millisecond)
 	}
