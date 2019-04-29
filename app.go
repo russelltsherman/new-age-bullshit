5e0a8a709427f26be6b46389f3662ae7ef25680d
Russell <russell.t.sherman@gmail.com>
2019-04-28 23:34:14 -0500

    add execution notes

--- app.go
+++ app.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"os/exec"
 	"time"
@@ -12,13 +12,37 @@ import (
 func main() {
 	for {
 		sentence := bs.Sentence()
-		fmt.Println(sentence)
+		log.Println(sentence)
 
-		cmd := exec.Command("say", sentence)
-		err := cmd.Run()
-		if err != nil {
-			log.Fatalf("cmd.Run() failed with %s\n", err)
-		}
-		time.Sleep(800 * time.Millisecond)
+		// executing using osx say
+		// say is good sound but only available on OSX
+		// cmd := exec.Command("say", sentence)
+		// err := cmd.Run()
+		// if err != nil {
+		// 	log.Fatalf("cmd.Run() failed with %s\n", err)
+		// }
+
+		// executing using espeak
+		// espeak frequently freezes
+		// cmd := exec.Command("espeak", "-ven+f3", "-k5", "-s150", sentence)
+		// err := cmd.Run()
+		// if err != nil {
+		// 	log.Fatalf("cmd.Run() failed with %s\n", err)
+		// }
+
+		// executing using festival
+		// festival is most stable playback tested
+		c1 := exec.Command("echo", sentence)
+		c2 := exec.Command("festival", "--tts")
+		r, w := io.Pipe()
+		c1.Stdout = w
+		c2.Stdin = r
+		c1.Start()
+		c2.Start()
+		c1.Wait()
+		w.Close()
+		c2.Wait()
+
+		time.Sleep(1000 * time.Millisecond)
 	}
 }
