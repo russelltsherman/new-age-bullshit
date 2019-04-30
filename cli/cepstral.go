a4d47d8eed9460e9dafebebe603af0eee19cf16e
Russell <russell.t.sherman@gmail.com>
2019-04-29 22:04:38 -0500

    add options flags for cepstral

--- cli/cepstral.go
+++ cli/cepstral.go
@@ -10,17 +10,23 @@ import (
 
 // CepstralCommand returns a cobra command
 func CepstralCommand() *cobra.Command {
+	var voice string
+
 	cmd := &cobra.Command{
 		Use:   "cepstral",
 		Short: "pipe output to Cepstral TTS",
 		Run: func(cmd *cobra.Command, args []string) {
 			for {
 				sentence := bs.Sentence()
-				c1 := exec.Command("aoss", "swift", sentence)
+				c1 := exec.Command("aoss", "swift", "-n", voice, sentence)
 				c1.Run()
 				time.Sleep(1000 * time.Millisecond)
 			}
 		},
 	}
+	// https://www.cepstral.com
+	// options depend on which voices you have licensed and installed
+	cmd.Flags().StringVarP(&voice, "voice", "v", "David", "the voice to use")
+
 	return cmd
 }
